72d7d5fe06ab4a34a1a571df548034618b30f811
Julius Figge <julius@figge.eu>
2020-06-01 00:14:28 +0200

    hack navbar toggle transparency

--- src/js/tweaks.js
+++ src/js/tweaks.js
@@ -2,5 +2,7 @@ $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        var $navbarnav = $(".navbar-nav");
+        $navbarnav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
