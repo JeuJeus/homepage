cbfb891c23c36355c92e416357331b582dd32ace
Julius Figge <julius@figge.eu>
2020-06-01 17:08:33 +0200

    fix concurrency

--- src/js/tweaks.js
+++ src/js/tweaks.js
@@ -1,8 +1,8 @@
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $nav.toggleClass('scrolled', $(this).scrollTop() > 10);
         var $navbarnav = $(".navbar-nav");
-        $navbarnav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $navbarnav.toggleClass('scrolled', $(this).scrollTop() > 10);
     });
 });
