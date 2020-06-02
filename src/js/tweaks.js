c12458135a296a6fa063895e5ad4c73c5d8f4d46
Julius Figge <julius@figge.eu>
2020-06-02 22:16:51 +0200

    fix navbar centering, collapse

--- src/js/tweaks.js
+++ src/js/tweaks.js
@@ -6,3 +6,17 @@ $(function () {
         $navbarnav.toggleClass('scrolled', $(this).scrollTop() > 10);
     });
 });
+
+$('.carousel-sync').carousel('cycle');
+$('.carousel-sync').on('click', '.carousel-control[data-slide]', function (ev) {
+    ev.preventDefault();
+    $('.carousel-sync').carousel($(this).data('slide'));
+});
+$('.carousel-sync').on('mouseover', function (ev) {
+    ev.preventDefault();
+    $('.carousel-sync').carousel('pause');
+});
+$('.carousel-sync').on('mouseleave', function (ev) {
+    ev.preventDefault();
+    $('.carousel-sync').carousel('cycle');
+});
